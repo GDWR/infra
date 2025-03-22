00c336ba01175458b2be6028f66b5bf205d5e25a
GDWR <gregory.dwr@gmail.com>
2025-03-22 15:41:38 +0000

    Forward ip packets from router

--- hosts/node.nix
+++ hosts/node.nix
@@ -11,12 +11,4 @@
   
   networking.dhcpcd.persistent = true;
   networking.dhcpcd.allowInterfaces = ["eth1"];
-  networking.dhcpcd.extraConfig = ''
-    noarp
-    noipv6
-
-    interface eth1
-    static routers=10.1.1.1
-    static domain_name_servers=10.1.1.1 1.1.1.1 8.8.8.8 8.8.4.4
-  '';
 }
--- hosts/router.nix
+++ hosts/router.nix
@@ -6,8 +6,26 @@
     extraGroups  = [ "wheel" ];
   };
 
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  
   networking.hostName = "router";
   networking.firewall.enable = false;
+  networking.nftables = {
+    enable = true;
+    tables.nat = {
+      family = "ip";
+      content = ''
+        chain prerouting {
+            type nat hook prerouting priority filter; policy accept;
+        }
+                    
+        chain postrouting {
+            type nat hook postrouting priority srcnat; policy accept;
+            oifname { "eth0" } masquerade
+        }
+      '';
+    };
+  };
   networking.interfaces.eth1 = {
     ipv4.addresses = [{
       address = "10.1.1.1";
@@ -22,6 +40,11 @@
       interface = "eth1";
       dhcp-range = [
         "10.1.1.2,10.1.1.254"
+      ];
+      server = [
+        "1.1.1.1"
+        "8.8.8.8"
+        "8.8.4.4"
       ];
     };
   };
